36992c7d3f622e9a830f45a02736383a4d0ceefc
Tanisha mittal <162045898+tanisha1608@users.noreply.github.com>
2024-07-15 00:15:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
-// Initialize Sudoku grid variable
-let sudokuGrid = [];
+var numSelected = null;
+var tileSelected = null;
 
-// Function to generate a random Sudoku grid
+var errors = 0;
+
+// Generate random Sudoku board and solution
+var { board, solution } = generateRandomSudoku();
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    // Digits 1-9
+    for (let i = 1; i <= 9; i++) {
+        let number = document.createElement("div");
+        number.id = i.toString();
+        number.innerText = i.toString();
+        number.addEventListener("click", selectNumber);
+        number.classList.add("number");
+        document.getElementById("digits").appendChild(number);
+    }
+
+    // Board 9x9
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            if (board[r][c] !== 0) {
+                tile.innerText = board[r][c].toString();
+                tile.classList.add("tile-start");
+            }
+            if (r === 2 || r === 5) {
+                tile.classList.add("horizontal-line");
+            }
+            if (c === 2 || c === 5) {
+                tile.classList.add("vertical-line");
+            }
+            tile.addEventListener("click", selectTile);
+            tile.classList.add("tile");
+            document.getElementById("board").append(tile);
+        }
+    }
+}
+
 function generateRandomSudoku() {
-    sudokuGrid = []; // Clear previous grid
-    const emptyPercentage = 40; // Adjust this to change the percentage of empty cells
+    const grid = [];
+    const solution = [];
+    const emptyPercentage = 50; // Adjust this to change the percentage of empty cells
 
     // Initialize the grid with empty cells
     for (let i = 0; i < 9; i++) {
-        sudokuGrid.push([]);
+        grid.push([]);
+        solution.push([]);
         for (let j = 0; j < 9; j++) {
-            sudokuGrid[i].push(0); // 0 represents an empty cell
+            grid[i].push(0); // 0 represents an empty cell
+            solution[i].push(0); // 0 represents an empty cell for solution
         }
     }
 
@@ -21,28 +65,30 @@ function generateRandomSudoku() {
                 let num;
                 do {
                     num = Math.floor(Math.random() * 9) + 1;
-                } while (!isValid(i, j, num));
-                sudokuGrid[i][j] = num;
+                } while (!isValid(grid, i, j, num));
+                grid[i][j] = num;
+                solution[i][j] = num; // Initially, solution is the same as grid
             }
         }
     }
 
-    // Generate the board with input fields
-    generateBoard();
+    // Solve the Sudoku grid to get the solution
+    solveSudoku(solution);
+
+    return { board: grid, solution: solution };
 }
 
-// Function to check if a number can be placed in a particular cell
-function isValid(row, col, num) {
+function isValid(grid, row, col, num) {
     // Check row
     for (let i = 0; i < 9; i++) {
-        if (sudokuGrid[row][i] === num) {
+        if (grid[row][i] === num) {
             return false;
         }
     }
 
     // Check column
     for (let i = 0; i < 9; i++) {
-        if (sudokuGrid[i][col] === num) {
+        if (grid[i][col] === num) {
             return false;
         }
     }
@@ -52,7 +98,7 @@ function isValid(row, col, num) {
     const startCol = Math.floor(col / 3) * 3;
     for (let i = startRow; i < startRow + 3; i++) {
         for (let j = startCol; j < startCol + 3; j++) {
-            if (sudokuGrid[i][j] === num) {
+            if (grid[i][j] === num) {
                 return false;
             }
         }
@@ -61,50 +107,91 @@ function isValid(row, col, num) {
     return true;
 }
 
-// Function to generate the Sudoku board with input fields
-function generateBoard() {
-    const board = document.querySelector('.board');
-    board.innerHTML = '';
+function selectNumber() {
+    if (numSelected !== null) {
+        numSelected.classList.remove("number-selected");
+    }
+    numSelected = this;
+    numSelected.classList.add("number-selected");
+}
 
-    // Populate the board with cells and input fields
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
+function selectTile() {
+    if (numSelected) {
+        if (this.innerText !== "") {
+            return;
+        }
 
-            // Create input fields for editable cells
-            if (sudokuGrid[i][j] === 0) {
-                const input = document.createElement('input');
-                input.type = 'text';
-                input.maxLength = 1;
-                input.classList.add('input-cell');
-                input.dataset.row = i;
-                input.dataset.col = j;
-                cell.appendChild(input);
-            } else {
-                cell.textContent = sudokuGrid[i][j];
+        let coords = this.id.split("-");
+        let r = parseInt(coords[0]);
+        let c = parseInt(coords[1]);
+
+        if (solution[r][c] === parseInt(numSelected.id)) {
+            this.innerText = numSelected.id;
+            if (checkSolution()) {
+                alert("Congratulations! You solved the Sudoku.");
             }
-
-            board.appendChild(cell);
+        } else {
+            errors += 1;
+            document.getElementById("errors").innerText = errors;
         }
     }
 }
 
-// Function to check the solution (not implemented in this basic example)
 function checkSolution() {
-    alert('Checking solution...');
+    let currentBoard = getCurrentBoard();
+    return compareArrays(currentBoard, solution);
 }
 
-// Event listener for input cells
-document.addEventListener('input', function(e) {
-    const input = e.target;
-    const row = parseInt(input.dataset.row);
-    const col = parseInt(input.dataset.col);
-    const value = parseInt(input.value);
+function getCurrentBoard() {
+    let currentBoard = [];
+    for (let r = 0; r < 9; r++) {
+        currentBoard.push([]);
+        for (let c = 0; c < 9; c++) {
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            currentBoard[r].push(tile.innerText === "" ? 0 : parseInt(tile.innerText));
+        }
+    }
+    return currentBoard;
+}
 
-    // Update the Sudoku grid with the user input
-    sudokuGrid[row][col] = value;
-});
+function compareArrays(arr1, arr2) {
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            if (arr1[i][j] !== arr2[i][j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
-// Initial generation of Sudoku board
-generateRandomSudoku();
+function solveSudoku(board) {
+    let emptySpot = findEmptySpot(board);
+    if (!emptySpot) {
+        return true; // No empty spots left, solution found
+    }
+
+    let [row, col] = emptySpot;
+    for (let num = 1; num <= 9; num++) {
+        if (isValid(board, row, col, num)) {
+            board[row][col] = num;
+            if (solveSudoku(board)) {
+                return true;
+            }
+            board[row][col] = 0; // Backtrack
+        }
+    }
+
+    return false; // No valid number found, backtrack
+}
+
+function findEmptySpot(board) {
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            if (board[r][c] === 0) {
+                return [r, c];
+            }
+        }
+    }
+    return null; // No empty spots left
+}
